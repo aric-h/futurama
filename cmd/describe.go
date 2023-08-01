376756b7c148871dd3db09b0c2f49eefba932769
Aric Hansen <aric.p.hansen@gmail.com>
2023-08-01 08:24:43 -0600

    updating command help text

--- cmd/describe.go
+++ cmd/describe.go
@@ -6,8 +6,8 @@ import (
 
 var describeCmd = &cobra.Command{
 	Use:   "describe",
-	Short: "Describe a Futurama episode (powered by Infosphere)",
-	Long:  "Describe the plot of a given Futurama episode",
+	Short: "Describe a Futurama episode (powered by Wikipedia)",
+	Long:  "Describe the plot of a user-defined Futurama episode",
 	Example: `  futurama describe episode --name "Space Pilot 3000"
   `,
 	Args: cobra.ExactArgs(1),
